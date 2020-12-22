2c249a4882fa49db4810ee7e2f965c9e3fbcac40
zzzdong <kuwater@163.com>
2020-12-22 18:36:08 +0800

    refator: modern rust
    1. use sled instead of rocksdb
    2. use anyhow instead of failure.
    3. update etcdv3client which use tonic 0.2
    4. use async

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use std::path::{Path, PathBuf};
 
-use etcdv3client::EtcdV3Client;
-use failure::Error;
-use rocksdb::{IteratorMode, DB};
-
+use anyhow::Result;
+use etcdv3client::EtcdClient;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -20,19 +18,25 @@ enum Opt {
 }
 
 #[derive(StructOpt, Debug)]
-struct CommOpts {
+struct EtcdOpts {
     #[structopt(long = "host", short = "h")]
     host: String,
     #[structopt(long = "port", short = "p")]
     port: u16,
-    #[structopt(long = "dir", short = "d", parse(from_os_str))]
-    dir: PathBuf,
+    #[structopt(long = "user", short = "u")]
+    user: Option<String>,
+    #[structopt(long = "password", short = "P")]
+    password: Option<String>,
+    #[structopt(long = "prefix")]
+    prefix: Option<String>,
 }
 
 #[derive(StructOpt, Debug)]
 struct DumpCmd {
     #[structopt(flatten)]
-    comm_opts: CommOpts,
+    etcd_opts: EtcdOpts,
+    #[structopt(long = "dir", short = "d", parse(from_os_str))]
+    dir: PathBuf,
     #[structopt(long = "all")]
     all: bool,
 }
@@ -40,15 +44,15 @@ struct DumpCmd {
 #[derive(StructOpt, Debug)]
 struct RestoreCmd {
     #[structopt(flatten)]
-    comm_opts: CommOpts,
+    etcd_opts: EtcdOpts,
+    #[structopt(long = "dir", short = "d", parse(from_os_str))]
+    dir: PathBuf,
 }
 
 #[derive(StructOpt, Debug)]
 struct PrintCmd {
-    #[structopt(long = "host", short = "h")]
-    host: String,
-    #[structopt(long = "port", short = "p")]
-    port: u16,
+    #[structopt(flatten)]
+    etcd_opts: EtcdOpts,
     #[structopt(long = "all")]
     all: bool,
 }
@@ -59,58 +63,80 @@ struct ReadCmd {
     dir: PathBuf,
 }
 
-fn main() -> Result<(), Error> {
+#[tokio::main]
+async fn main() -> Result<()> {
     env_logger::init();
 
     let opt = Opt::from_args();
 
     match opt {
-        Opt::Dump(o) => dump_data(&o.comm_opts.host, o.comm_opts.port, &o.comm_opts.dir, o.all)?,
-        Opt::Restore(o) => restore_data(&o.comm_opts.host, o.comm_opts.port, &o.comm_opts.dir)?,
-        Opt::Print(o) => print_data(&o.host, o.port, o.all)?,
+        Opt::Dump(o) => dump_data(&o.etcd_opts, &o.dir, o.all).await?,
+        Opt::Restore(o) => restore_data(&o.etcd_opts, &o.dir).await?,
+        Opt::Print(o) => print_data(&o.etcd_opts, o.all).await?,
         Opt::Read(o) => read_data(&o.dir)?,
     }
 
     Ok(())
 }
 
-fn dump_data(host: &str, port: u16, path: &Path, all: bool) -> Result<(), Error> {
-    let etcd_client = EtcdV3Client::new(host, port).unwrap();
-    let client = etcd_client.new_simple_kv();
+async fn connect_etcd(etcd_opts: &EtcdOpts) -> Result<EtcdClient> {
+    let mut auth: Option<(String, String)> = None;
+    if let Some(user) = &etcd_opts.user {
+        auth = Some((user.clone(), etcd_opts.password.clone().unwrap()));
+    }
 
-    let db = DB::open_default(path).unwrap();
+    let endpoint = format!("http://{}:{}", etcd_opts.host, etcd_opts.port);
 
-    let kvs = client.get_all()?;
+    let client = EtcdClient::new(vec![endpoint], auth).await?;
+
+    Ok(client)
+}
+
+async fn dump_data(etcd_opts: &EtcdOpts, path: &Path, all: bool) -> Result<()> {
+    let mut client = connect_etcd(etcd_opts).await?;
+
+    let tree = sled::open(path)?;
+    let mut batch = sled::Batch::default();
+
+    let kvs = match &etcd_opts.prefix {
+        Some(p) => client.get_with_prefix(p).await?,
+        None => client.all().await?,
+    };
+
     for kv in kvs {
         if all || kv.lease == 0 {
-            db.put(kv.key, kv.value).unwrap();
+            batch.insert(kv.key, kv.value);
         }
     }
 
-    db.flush()?;
+    tree.apply_batch(batch)?;
+    tree.flush()?;
 
     Ok(())
 }
 
-fn restore_data(host: &str, port: u16, path: &Path) -> Result<(), Error> {
-    let etcd_client = EtcdV3Client::new(host, port).unwrap();
-    let client = etcd_client.new_simple_kv();
+async fn restore_data(etcd_opts: &EtcdOpts, path: &Path) -> Result<()> {
+    let mut client = connect_etcd(etcd_opts).await?;
 
-    let db = DB::open_default(path).unwrap();
-    let iter = db.iterator(IteratorMode::Start);
+    let tree = sled::open(path)?;
 
-    for (key, value) in iter {
-        client.put_bytes_kv(key.to_vec(), value.to_vec())?;
+    for kv in tree.iter() {
+        if let Ok((key, value)) = kv {
+            client.put(key.to_vec(), value.to_vec()).await?;
+        }
     }
 
     Ok(())
 }
 
-fn print_data(host: &str, port: u16, all: bool) -> Result<(), Error> {
-    let etcd_client = EtcdV3Client::new(host, port).unwrap();
-    let client = etcd_client.new_simple_kv();
+async fn print_data(etcd_opts: &EtcdOpts, all: bool) -> Result<()> {
+    let mut client = connect_etcd(etcd_opts).await?;
 
-    let kvs = client.get_all()?;
+    let kvs = match &etcd_opts.prefix {
+        Some(p) => client.get_with_prefix(p).await?,
+        None => client.all().await?,
+    };
+
     for kv in kvs {
         if all || kv.lease == 0 {
             println!(
@@ -124,16 +150,17 @@ fn print_data(host: &str, port: u16, all: bool) -> Result<(), Error> {
     Ok(())
 }
 
-fn read_data(path: &Path) -> Result<(), Error> {
-    let db = DB::open_default(path).unwrap();
-    let iter = db.iterator(IteratorMode::Start);
+fn read_data(path: &Path) -> Result<()> {
+    let tree = sled::open(path)?;
 
-    for (key, value) in iter {
-        println!(
-            "{} => {}",
-            String::from_utf8_lossy(&key),
-            String::from_utf8_lossy(&value)
-        );
+    for kv in tree.iter() {
+        if let Ok((key, value)) = kv {
+            println!(
+                "{} => {}",
+                String::from_utf8_lossy(&key),
+                String::from_utf8_lossy(&value)
+            );
+        }
     }
 
     Ok(())
