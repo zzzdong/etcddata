e9305a34c01afa724451269aad3bf9b6f1e5fdcc
zzzdong <kuwater@163.com>
2022-06-30 17:09:28 +0800

    ci: fix build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-RELEASE_DIR=build/release/etcddata-${GITHUB_REF_NAME}-${RUNNER_OS}-${RUNNER_ARCH}
+OUT_DIR=etcddata-${GITHUB_REF_NAME}-${RUNNER_OS}-${RUNNER_ARCH}
 
-mkdir -pv ${RELEASE_DIR}
+RELEASE_DIR=build/release/
 
-cargo build --release && cp -v target/release/etcddata ${RELEASE_DIR} && cd ${RELEASE_DIR} && tar czvf ${RELEASE_DIR}.tar.gz ${RELEASE_DIR}
+mkdir -pv ${RELEASE_DIR}/${OUT_DIR}
+
+cargo build --release && cp -v target/release/etcddata ${RELEASE_DIR}/${OUT_DIR} && cd ${RELEASE_DIR} && tar czvf ${OUT_DIR}.tar.gz ${OUT_DIR} && rm -vr ${OUT_DIR}
